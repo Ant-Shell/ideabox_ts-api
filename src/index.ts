4031edb116d32542e589f3defb33005a6748d605
Anthony Shellman <atshellman@gmail.com>
2023-03-18 12:58:55 -0600

    Install types express

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
-const express:Function = require('express')
-const cors:Function = require('cors')
-const app:any = express()
-const port:number = 3001
+// const express:Function = require('express')
+// const cors:Function = require('cors')
+// const app:any = express()
+// const port:number = 3001
+
+import express, { Express, Request, Response } from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app: Express = express()
+const port = process.env.PORT
+
 
 app.get('/', (request:string, response:any) => {
   response.send('Hello World!')
